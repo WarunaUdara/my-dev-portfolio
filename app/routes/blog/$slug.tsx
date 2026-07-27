7f88b4e4e5116f08fc8647c6ef2d5523bc310762
WarunaUdara <warunaudarasam2003@gmail.com>
2026-07-28 03:01:32 +0530

    feat(blog): add ScrollFrost background and 2-sided Scales ruler strips to blog routes

--- app/routes/blog/$slug.tsx
+++ app/routes/blog/$slug.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import BlogPost from "../../sections/BlogPost";
 import Footer from "../../sections/Footer";
 import { NavBar } from "../../ui/TubelightNavbar";
+import ScrollFrost from "@/components/canvasui/ScrollFrost";
+import Scales from "@/components/ui/scales";
 import { IconHome, IconUser, IconBriefcase, IconFileText } from "@tabler/icons-react";
 
 export const Route = createFileRoute("/blog/$slug")({
@@ -18,10 +20,22 @@ function BlogPostRoutePage() {
   ];
 
   return (
-    <div className="min-h-screen bg-black text-white scroll-smooth">
-      <div className="pt-12">
+    <div className="relative min-h-screen bg-black text-white selection:bg-white selection:text-black overflow-x-hidden">
+      {/* 2-Sided Scales Ruler Strips (Matching Uses & Work Page Layout) */}
+      <div className="fixed top-0 bottom-0 left-3 sm:left-6 md:left-10 w-6 sm:w-8 z-20 border-x border-neutral-800/80 pointer-events-none hidden sm:block">
+        <Scales orientation="diagonal" size={8} className="w-full opacity-60" />
+      </div>
+      <div className="fixed top-0 bottom-0 right-3 sm:right-6 md:right-10 w-6 sm:w-8 z-20 border-x border-neutral-800/80 pointer-events-none hidden sm:block">
+        <Scales orientation="diagonal" size={8} className="w-full opacity-60" />
+      </div>
+
+      {/* Dynamic ScrollFrost Background */}
+      <ScrollFrost height="h-[600px]" />
+
+      <div className="relative z-10 pt-12">
         <BlogPost slug={slug} />
       </div>
+
       <Footer />
       <NavBar items={navItems} />
     </div>
